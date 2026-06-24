4f194e76529f265ff3a632bce6e42179ceacdfd4
Arushi Shandilya <arushishandilya4@gmail.com>
2026-06-25 00:06:50 +0530

    Fix model filename

--- app.py
+++ app.py
@@ -65,3 +65,6 @@ if __name__ == '__main__':
     # Use port Render assigns, or default to 5000 locally
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+
+    
